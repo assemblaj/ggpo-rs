7fd8e21ac4617679352863ac26cbb158e74c6bc7
assemblaj <assemblaj@users.noreply.github.com>
2019-10-16 23:11:31 -0400

    adds bindgeninstructed by tutorial

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+/* #[cfg(test)]
+mod tests {
+    use super::*;
+    use std::mem;
+
+    #[test]
+    fn ggpo_basic_test() {
+        unsafe {
+            assert_eq!(GGPO_MAX_PLAYERS, 4);
+        }
+    }
+} */
+
+
+
+
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include "ggponet.h"
