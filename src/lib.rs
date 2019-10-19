fcc6d5900f3268f466be151acbc8a1f674c1ad3e
0xCA2 <assemblaj@users.noreply.github.com>
2019-10-19 17:22:15 -0400

    Adds stub functions for callbacks

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,30 @@ mod tests {
     use super::*;
     use std::mem;
     use std::ffi::CString;
+    use std::os::raw::*;
+
+    // Stub callback functions 
+    unsafe extern "C" fn begin_game_callback (game: *const c_char) -> bool {
+        return true; 
+    }
+    
+    unsafe extern "C" fn advance_frame_callback(flags: c_int) -> bool {
+        return true; 
+    }
+
+    unsafe extern "C" fn load_game_state_callback(buffer: *mut c_uchar, len: c_int) -> bool {
+        return true;
+    }
+
+    unsafe extern "C" fn save_game_state_callback(buffer: *mut *mut c_uchar, len: *mut c_int, checksum: *mut c_int, frame: c_int) -> bool {
+        return true;
+    }
+    
+    unsafe extern "C" fn free_buffer_callback(buffer: *mut c_void) {}
+
+    unsafe extern "C" fn on_event_callback(info: *mut GGPOEvent) -> bool {
+        return true;
+    }
 
     #[test]
     fn ggpo_basic_test() {
@@ -17,23 +41,22 @@ mod tests {
             let result: GGPOErrorCode;
             let cb: *mut GGPOSessionCallbacks = std::ptr::null_mut();
             let name_str = CString::new("Test App").expect("CString::new failed");
-
-            
+                        
             /* fill in all callback functions */
-            // cb.begin_game = vw_begin_game_callback;
-            // cb.advance_frame = vw_advance_frame_callback;
-            // cb.load_game_state = vw_load_game_state_callback;
-            // cb.save_game_state = vw_save_game_state_callback;
-            // cb.free_buffer = vw_free_buffer;
-            // cb.on_event = vw_on_event_callback;
+            (*cb).begin_game = Some(begin_game_callback); 
+            (*cb).advance_frame = Some(advance_frame_callback); 
+            (*cb).load_game_state = Some(load_game_state_callback); 
+            (*cb).save_game_state = Some(save_game_state_callback); 
+            (*cb).free_buffer = Some(free_buffer_callback); 
+            (*cb).on_event = Some(on_event_callback); 
 
             /* Start a new session */
-            result = ggpo_start_session(&mut ggpo,         // the new session object
-                                        cb,           // our callbacks
-                                        name_str.as_ptr(),    // application name
-                                        2,             // 2 players
-                                        mem::size_of::<i32>() as i32,   // size of an input packet
-                                        8001);         // our local udp port
+            result = ggpo_start_session(&mut ggpo,                    // the new session object
+                                        cb,                           // our callbacks
+                                        name_str.as_ptr(),            // application name
+                                        2,                            // 2 players
+                                        mem::size_of::<i32>() as i32, // size of an input packet
+                                        8001);                        // our local udp port
 
             assert_eq!(result, GGPOErrorCode_GGPO_OK);
         }
