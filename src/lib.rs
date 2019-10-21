9298204b6364d077c4309e32580231cbd17190d7
0xCA2 <assemblaj@users.noreply.github.com>
2019-10-20 21:56:32 -0400

    Adds code for testing ggpo_add_player [unfinished]

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+extern crate winapi;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
@@ -10,6 +11,8 @@ mod tests {
     use std::ffi::CString;
     use std::mem;
     use std::os::raw::*;
+    use std::ptr::copy_nonoverlapping;
+    use winapi::*;
 
     // Stub callback functions
     unsafe extern "C" fn begin_game_callback(game: *const c_char) -> bool {
@@ -47,9 +50,7 @@ mod tests {
     */
     fn ggpo_basic_test() {
         unsafe {
-            /*
-                Creating the GGPOSession Object
-            */
+            // Creating the GGPOSession Object
             let mut ggpo_ptr: *mut GGPOSession = std::ptr::null_mut();
             let mut ggpo = GGPOSession { _unused: [0; 0] };
             ggpo_ptr = &mut ggpo;
@@ -80,9 +81,59 @@ mod tests {
 
             assert_eq!(result, GGPOErrorCode_GGPO_OK);
 
-            result = ggpo_close_session(ggpo_ptr);
+            // need to test that this works at the end of the function
+            //result = ggpo_close_session(ggpo_ptr);
+            //assert_eq!(result, GGPOErrorCode_GGPO_OK);
 
-            assert_eq!(result, GGPOErrorCode_GGPO_OK);
+            // Sending Player Locations
+            let mut ip_address_arr: [c_char; 32] = [0; 32];
+            let mut ip_address_str = CString::new("192.168.0.100").unwrap();
+            std::ptr::copy_nonoverlapping(
+                ip_address_arr.as_mut_ptr(),
+                ip_address_str.into_raw(),
+                ip_address_arr.len(),
+            );
+
+            // TODO: This shoud use the default trait that returns
+            // a different for local and remote
+            let mut p1: GGPOPlayer = GGPOPlayer {
+                player_num: 1,
+                size: mem::size_of::<GGPOPlayer>() as i32,
+                type_: GGPOPlayerType_GGPO_PLAYERTYPE_LOCAL,
+                u: GGPOPlayer__bindgen_ty_1 {
+                    local: GGPOPlayer__bindgen_ty_1__bindgen_ty_1 { _address: 0 },
+                },
+            };
+            let mut p2: GGPOPlayer = GGPOPlayer {
+                player_num: 2,
+                size: mem::size_of::<GGPOPlayer>() as i32,
+                type_: GGPOPlayerType_GGPO_PLAYERTYPE_REMOTE,
+                u: GGPOPlayer__bindgen_ty_1 {
+                    remote: GGPOPlayer__bindgen_ty_1__bindgen_ty_2 {
+                        ip_address: ip_address_arr,
+                        port: 8001,
+                    },
+                },
+            };
+            let mut player_handles: [GGPOPlayerHandle; 2] = [0; 2];
+
+            let mut data: winapi::um::winsock2::WSADATA = std::mem::zeroed();
+            let ws_res = winapi::um::winsock2::WSAStartup(
+                winapi::shared::minwindef::MAKEWORD(2, 2), // minwindef::MAKEWORD(2 , 2), 
+                &mut data);
+
+            // Okay, sometimes I get an error from it, sometimes it fails. 
+            // TODO: Fix that.
+            if ws_res != 0 {
+                panic!("Error from WSAStartup"); 
+            }
+
+            // TODO: Fix error that this creates  
+            //result = ggpo_add_player(ggpo_ptr, &mut p1, &mut player_handles[0]);
+            //assert_eq!(result, GGPOErrorCode_GGPO_OK);
+
+            //result = ggpo_add_player(ggpo_ptr, &mut p2, &mut player_handles[1]);
+            //assert_eq!(result, GGPOErrorCode_GGPO_OK);
         }
     }
 }
